3a1be1755ebd32eedd22a6e485c0b8fe9194320b
eepitapplication@gmail.com <eepitapplication@gmail.com>
2023-01-13 16:13:25 +0300

    angular excercise event and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Excercise';
+  firstName="Gedef";
+  lastName="Shibabaw";
+  isDisabled=true;
+  myClass="red";
+
+  cssStringVar: string= 'red size20';
+
+  getClass(){
+    return 'red';
+  }
+  hasError(){
+    return true;
+  
+  }
+
+  getBackground(){
+
+    return 'Green';
+  }
+  onSave(){
+    alert("Registerd Succuessfully")
+  }
+
+
 }
